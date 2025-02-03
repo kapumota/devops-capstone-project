b144baa501ce67550646c1134d5ab20bd437be4a
kapumota <121532729+kapumota@users.noreply.github.com>
2025-02-02 23:19:38 -0500

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -9,11 +9,17 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 
+# Import Talisman from Flask-Talisman for security headers
+from flask_talisman import Talisman
+
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
 
-# Import the routes After the Flask app is created
+# Initialize Flask-Talisman for security headers
+talisman = Talisman(app)
+
+# Import the routes after the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
 
