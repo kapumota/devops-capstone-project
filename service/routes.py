74a2fd53ef812ed99d3bba16f7251366de9deffa
kapumota <121532729+kapumota@users.noreply.github.com>
2025-02-02 21:58:27 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -3,10 +3,12 @@ Account Service
 
 This microservice handles the lifecycle of Accounts
 """
-from flask import jsonify, request, make_response, abort, url_for
+
+from flask import jsonify, request, make_response, abort
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
+
 
 ############################################################
 # Health Endpoint
@@ -15,6 +17,7 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
+
 
 ######################################################################
 # GET INDEX
@@ -31,6 +34,7 @@ def index():
         ),
         status.HTTP_200_OK,
     )
+
 
 ######################################################################
 # CREATE A NEW ACCOUNT
@@ -54,6 +58,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -68,6 +73,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # READ AN ACCOUNT
@@ -86,6 +92,7 @@ def get_account(account_id):
             f"Account with id [{account_id}] could not be found."
         )
     return jsonify(account.serialize()), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -107,6 +114,7 @@ def update_accounts(account_id):
     account.update()
     return jsonify(account.serialize()), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -121,6 +129,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
