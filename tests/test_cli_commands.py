9abcc2071b0526ec8352e9c32164bb47a2b8f842
kapumota <121532729+kapumota@users.noreply.github.com>
2025-02-03 00:41:31 -0500

    Update test_cli_commands.py

--- tests/test_cli_commands.py
+++ tests/test_cli_commands.py
@@ -1,12 +1,11 @@
 """
-CLI Command Extensions for Flask
+CLI Command Extensions for Flask - Test Cases
 """
 import os
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
 from click.testing import CliRunner
 from service.common.cli_commands import db_create
-
 
 class TestFlaskCLI(TestCase):
     """Test Flask CLI Commands"""
@@ -17,7 +16,13 @@ class TestFlaskCLI(TestCase):
     @patch('service.common.cli_commands.db')
     def test_db_create(self, db_mock):
         """It should call the db-create command"""
-        db_mock.return_value = MagicMock()
-        with patch.dict(os.environ, {"FLASK_APP": "service:app"}, clear=True):
+        # Mock out db.create_all() and db.session.commit()
+        db_mock.create_all = MagicMock()
+        db_mock.session = MagicMock()
+        db_mock.session.commit = MagicMock()
+
+        # Force FLASK_APP to point to service/__init__.py
+        with patch.dict(os.environ, {"FLASK_APP": "service.__init__:app"}, clear=True):
             result = self.runner.invoke(db_create)
-            self.assertEqual(result.exit_code, 0)
+            print("CLI OUTPUT:", result.output)
+            self.assertEqual(result.exit_code, 0, f"Command failed: {result.output}")
