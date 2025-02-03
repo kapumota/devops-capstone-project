e9578cb43b5451f55738dd2900da522b7a88e0e8
kapumota <121532729+kapumota@users.noreply.github.com>
2025-02-03 00:16:46 -0500

    Update test_cli_commands.py

--- tests/test_cli_commands.py
+++ tests/test_cli_commands.py
@@ -1,9 +1,12 @@
+"""
+CLI Command Extensions for Flask
+"""
 import os
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from click.testing import CliRunner
 from service.common.cli_commands import db_create
-from service.routes import app
+
 
 class TestFlaskCLI(TestCase):
     """Test Flask CLI Commands"""
@@ -11,12 +14,10 @@ class TestFlaskCLI(TestCase):
     def setUp(self):
         self.runner = CliRunner()
 
-    @patch('service.common.cli_commands.db_create.__globals__["init_db"]')
-    def test_db_create(self, mock_init_db):
+    @patch('service.common.cli_commands.db')
+    def test_db_create(self, db_mock):
         """It should call the db-create command"""
-        with patch.dict(os.environ, {"FLASK_APP": "service:app", "DATABASE_URI": "sqlite://"}):
-            with app.app_context():
-                result = self.runner.invoke(db_create, [], standalone_mode=False)
-
-        mock_init_db.assert_called_once()             # or assert_called_once_with(app) / current_app
-        self.assertEqual(result.exit_code, 0)
+        db_mock.return_value = MagicMock()
+        with patch.dict(os.environ, {"FLASK_APP": "service:app"}, clear=True):
+            result = self.runner.invoke(db_create)
+            self.assertEqual(result.exit_code, 0)
