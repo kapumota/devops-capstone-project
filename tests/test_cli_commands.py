0066fe12ad296908a9d10703d302978ee6cdec81
kapumota <121532729+kapumota@users.noreply.github.com>
2025-02-03 00:00:37 -0500

    Update test_cli_commands.py

--- tests/test_cli_commands.py
+++ tests/test_cli_commands.py
@@ -11,14 +11,14 @@ class TestFlaskCLI(TestCase):
     def setUp(self):
         self.runner = CliRunner()
 
-    @patch('service.common.cli_commands.db.create_all')
+    @patch('service.models.db.create_all')
     def test_db_create(self, mock_create_all):
         """It should call the db-create command"""
-        # When called, mock_create_all will do nothing (i.e. succeed)
+        # mock_create_all will now patch the method as used by db_create
         with patch.dict(os.environ, {"FLASK_APP": "service:app"}):
             with app.app_context():
-                # Use standalone_mode=False so that exceptions are re‑raised
+                # Using standalone_mode=False lets exceptions propagate
                 result = self.runner.invoke(db_create, [], standalone_mode=False)
-        # Ensure the command actually called db.create_all()
+        # Check that create_all was indeed called
         mock_create_all.assert_called_once()
         self.assertEqual(result.exit_code, 0)
